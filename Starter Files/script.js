59c7d66db775ea823760d98ce4d927236ce2803d
Pradeep4157 <krp18539@gmail.com>
2026-01-29 11:10:32 +0530

    started with js proj..

---
+++ Starter Files/script.js
@@ -0,0 +1,20 @@
+/*
+    we i click on search button, it should give me all the movies that are of that category ?
+
+    so i search category .. 
+
+    maybe when i search about something then it is going to 
+
+    iterate over all the movies and if this matches then it
+
+    is going to send that movie data.. 
+
+    storing and removing movies from localStorage is not that 
+
+    difficult 
+
+        
+
+*/
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=603248ba";
